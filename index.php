006c0d50fa28e1002252ab7f3a3a1b1e19724d16
Qinghang Zhang <Qzhang9@mail.greenriver.edu>
2020-04-24 16:31:14 -0400

    index file

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+ini_set("display_errors",1);
+error_reporting(E_ALL);
+
+//require autoload file
+require_once('vendor/autoload.php');
+
+//Create an instance of the Base class
+$f3 = Base::instance();
+
+//default route
+$f3->route('GET /',function(){
+    echo '<h1>Hello World</h1>';
+    /*$view = new Template();
+    echo $view->render
+    ('views/home.html');*/
+
+
+});
+
+//run fat free (last things)
+$f3->run();
